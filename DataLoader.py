30d8999f47aa1ae9f5218dd409bfab2fa1b5d771
Nimbus8101 <elijahreyna03@gmail.com>
2025-04-15 14:32:34 -0500

    project setup
    The code pulls data from the json file and stores the relevant fields

--- DataLoader.py
+++ DataLoader.py
@@ -3,29 +3,49 @@ import numpy as np
 import json
 import pymongo
 
-def load_dataset():
+def load_dataset_into_mongodb(json_file, client_url, db_name, collection_name):
     """
-        Loads the dataset from games.csv
+        Loads the dataset from the json file and stores it into a MongoDB database.
+        
+        Parameters:
+        json_file (str): Path to the JSON file.
+        client_url (str): MongoDB client URL.
+        db_name (str): Name of the database.
+        collection_name (str): Name of the collection.
+        
     """
 
-    with open('games.json', encoding="utf8") as data_file:    
-        y = json.load(data_file)
-
-    # the result is a Python dictionary:
-    #for key in y:
-        #print(y[key]["name"])
+    # Open the JSON file and load the data into a dictionary
+    with open(json_file, encoding="utf8") as data_file:    
+        data_dict = json.load(data_file)
     
-    relevant_data = parse_relevant_data(y)
+    # Parse the relevant data from the dictionary
+    relevant_data = parse_relevant_data(data_dict)
     
-    df = pd.DataFrame(relevant_data)
+    # Create a local database
+    client = pymongo.MongoClient(client_url)
+    db = client[db_name]
+    collection = db[collection_name]
     
-    print(df)
-
-    return df
+    # If the database exists, create it. Otherwise, leave it alone    
+    if collection_name in client.list_database_names():
+        print("The database exists.")
+    else:
+        collection.insert_many(relevant_data)
+        
+        print("mongodb thing: ", client.list_database_names())
+        
+    client.close()
 
 def parse_relevant_data(data):
     """
-        Parses the relevant data from the JSON file.
+        Parses the relevant data from the JSON file and returns a list of dictionaries.
+        
+        Parameters:
+        data (dict): The JSON data loaded into a dictionary.
+        
+        Returns:
+        list: A list of dictionaries containing the relevant data.
     """
     
     relevant_data = []
@@ -47,13 +67,13 @@ def parse_relevant_data(data):
             'metacritic_score': game_data['metacritic_score'],
             'achievements': game_data['achievements'],
             'recommendations': game_data['recommendations'],
-            'supported_languages': game_data['supported_languages'], #could be a list
-            'full_audio_languages': game_data['full_audio_languages'], #could be a list
-            'developers': game_data['developers'], #could be a list
-            'publishers': game_data['publishers'], #could be a list
-            'categories': game_data['categories'], #could be a list
-            'genres': game_data['genres'], #could be a list
-            'tags': game_data['tags'], #could be a list
+            'supported_languages': game_data['supported_languages'], 
+            'full_audio_languages': game_data['full_audio_languages'], 
+            'developers': game_data['developers'], 
+            'publishers': game_data['publishers'], 
+            'categories': game_data['categories'], 
+            'genres': game_data['genres'], 
+            'tags': game_data['tags'], 
             'user_score': game_data['user_score'],
             'score_rank': game_data['score_rank'],
             'positive_reviews': game_data['positive'],
@@ -64,39 +84,52 @@ def parse_relevant_data(data):
             'median_playtime_forever': game_data['median_playtime_forever'],
             'median_playtime_2weeks': game_data['median_playtime_2weeks'],
             'peak_ccu': game_data['peak_ccu'],
-            'tags': game_data['tags'] #could be a list
+            'tags': game_data['tags'] 
         })
         
     return relevant_data
 
-def storeData(data_frame):
+def pull_collection_from_mongodb(client_url, db_name, collection_name):
     """
-        Stores the data in a MongoDB database.
+    Pulls the collection from the MongoDB database and returns it as a DataFrame.
+
+    Args:
+        client_url (string): The URL of the MongoDB client
+        db_name (string): The name of the database
+        collection_name (string): The name of the collection
+
+    Returns:
+        _type_: _description_
     """
     
-    # Create a local database
-    client = pymongo.MongoClient("mongodb://localhost:27017/")
-    db = client["games_database"]
-    collection = db["games"]
+    # Access the local database
+    client = pymongo.MongoClient(client_url)
+    db = client[db_name]
+    collection = db[collection_name]
     
-    # If the database exists, create it. Otherwise, leave it alone    
-    if "games" in client.list_database_names():
-        print("The database exists.")
+    # If the database exists, pull the document  
+    if collection_name in client.list_database_names():
+        document = db.games
     else:
-        data_dict = data_frame.to_dict(orient='records')
-        
-        collection.insert_many(data_dict)
-        
-        print("mongodb thing: ", client.list_database_names())
+        document = None
         
     client.close()
     
+    return document
+
 
 def main():
-    data = load_dataset()
+    DATA_FILE = "games.json"
+    CLIENT_URL = "mongodb://localhost:27017/"
+    DB_NAME = "games_database"
+    COLLECTION_NAME = "games"
     
-    storeData(data)
+    # Load the dataset into MongoDB
+    load_dataset_into_mongodb(DATA_FILE, CLIENT_URL, DB_NAME, COLLECTION_NAME)
     
+    data = pd.DataFrame(pull_collection_from_mongodb(CLIENT_URL, DB_NAME, COLLECTION_NAME))
+    
+    # Process Data Here
     print(data.head())
 
 if __name__ == "__main__":
